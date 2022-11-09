6bed246df696bca0e1dc55348c7e5d14d8f5dd29
Staszek <stasiu94pro@icload.com>
2022-11-09 08:02:09 +0100

    fix: hide nav when scrolling

--- src/components/navigationBar/NavigationBar.vue
+++ src/components/navigationBar/NavigationBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="burger" class="overlay-burger" @click="toggleBurger"></div>
-  <nav>
+  <nav :class="{ 'hidden-navbar': !showNavbar && !burger }" id="navbar">
     <div class="logo">
       <img src="/images/burger-logo.svg" />
       <h1>Dream Burger</h1>
@@ -22,7 +22,7 @@
         <span></span>
       </div>
     </div>
-    <div class="burger-menu" :class="{ closeBurger: !this.burger }">
+    <div class="burger-menu" :class="{ closeBurger: !burger }">
       <router-link to="/"><span>🍔</span>Home</router-link>
       <router-link to="/burger-creator"
         ><span>🍔</span>Create burger</router-link
@@ -35,15 +35,40 @@
 <script>
 import "./navigationBar.scss";
 
+const OFFSET = 60;
 export default {
   data() {
     return {
       burger: false,
+      showNavbar: true,
+      lastScrollPosition: 0,
+      scrollValue: 0,
     };
+  },
+  mounted() {
+    this.lastScrollPosition = window.pageYOffset;
+    window.addEventListener("scroll", this.onScroll);
+    const viewportMeta = document.createElement("meta");
+    viewportMeta.name = "viewport";
+    viewportMeta.content = "width=device-width, initial-scale=1";
+    document.head.appendChild(viewportMeta);
+  },
+  beforeUnmount() {
+    window.removeEventListener("scroll", this.onScroll);
   },
   methods: {
     toggleBurger() {
       this.burger = !this.burger;
+    },
+    onScroll() {
+      if (window.pageYOffset < 0) {
+        return;
+      }
+      if (Math.abs(window.pageYOffset - this.lastScrollPosition) < OFFSET) {
+        return;
+      }
+      this.showNavbar = window.pageYOffset < this.lastScrollPosition;
+      this.lastScrollPosition = window.pageYOffset;
     },
   },
 };
